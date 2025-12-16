52694a147527079756748015be5a592357500273
Leon <147289645+LeoninCS@users.noreply.github.com>
2025-12-16 18:46:24 +0800

    refactor:请求结构体放到entity

--- internal/account/entity.go
+++ internal/account/entity.go
@@ -6,3 +6,41 @@ type Account struct {
 	Password string `json:"-"`
 	Token    string `json:"-"`
 }
+
+type CreateAccountRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type RenameRequest struct {
+	NewUsername string `json:"new_username"`
+}
+
+type FindByIDRequest struct {
+	ID uint `json:"id"`
+}
+
+type FindByIDResponse struct {
+	ID       uint   `json:"id"`
+	Username string `json:"username"`
+}
+
+type FindByUsernameRequest struct {
+	Username string `json:"username"`
+}
+
+type FindByUsernameResponse struct {
+	ID       uint   `json:"id"`
+	Username string `json:"username"`
+}
+
+type ChangePasswordRequest struct {
+	Username    string `json:"username"`
+	OldPassword string `json:"old_password"`
+	NewPassword string `json:"new_password"`
+}
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
